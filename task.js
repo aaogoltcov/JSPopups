0e944cba77dd429dd3c5b54276add580307bd168
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-08-03 23:47:05 +0300

    some improvements

--- task.js
+++ task.js
@@ -1,49 +1,49 @@
 'use strict'
 
+let popupsArray = Array.from(document.querySelectorAll('div.modal__close_times'))
+
 // version 1
 // popup close function
 function closePopup() {
-    for (let modal in Array.from(document.querySelectorAll('div.modal__close_times'))) {
-        if (document.querySelectorAll('div.modal__close_times')[modal].closest('div.modal').classList.contains('modal_active')) {
-            document.querySelectorAll('div.modal__close_times')[modal].onclick = function () {
-                document.querySelectorAll('div.modal__close_times')[modal].closest('div.modal').className = 'modal';
-            };
-        }
+    popupsArray.forEach(function (modal) {
+        modal.closest('div.modal').classList.contains('modal_active') ? modal.onclick = function () {
+            modal.closest('div.modal').className = 'modal'} : '';
+    });
+};
 
-    }
-
-}
 // start with modal_main popup
-document.getElementById('modal_main').className = 'modal modal_active';
+let modalMain = document.getElementById('modal_main')
+let modalSuccess = document.getElementById('modal_success')
+modalMain.className = 'modal modal_active';
 
 // open modal_success with button
 document.querySelector('div.modal a.btn_danger').onclick = function () {
-    document.getElementById('modal_main').className = 'modal';
-    document.getElementById('modal_success').className = 'modal modal_active';
+    modalMain.className = 'modal';
+    modalSuccess.className = 'modal modal_active';
 
     // close popup
     closePopup();
-
 };
 
 // close popup
 closePopup();
 
 
-// version 2
+// // version 2
+// // popup close function
 // setInterval(function closePopup() {
-//     for (let modal in Array.from(document.querySelectorAll('div.modal__close_times'))) {
-//         if (document.querySelectorAll('div.modal__close_times')[modal].closest('div.modal').classList.contains('modal_active')) {
-//             document.querySelectorAll('div.modal__close_times')[modal].onclick = function () {
-//                 document.querySelectorAll('div.modal__close_times')[modal].closest('div.modal').className = 'modal';
-//             };
-//         };
-//     };
+//     popupsArray.forEach(function (modal) {
+//         modal.closest('div.modal').classList.contains('modal_active') ? modal.onclick = function () {
+//             modal.closest('div.modal').className = 'modal'} : '';
+//     });
 // }, 500);
 //
-// document.getElementById('modal_main').className = 'modal modal_active';
+// // start with modal_main popup
+// let modalMain = document.getElementById('modal_main')
+// let modalSuccess = document.getElementById('modal_success')
+// modalMain.className = 'modal modal_active';
 //
 // document.querySelector('div.modal a.btn_danger').onclick = function () {
-//     document.getElementById('modal_main').className = 'modal';
-//     document.getElementById('modal_success').className = 'modal modal_active';
+//     modalMain.className = 'modal';
+//     modalSuccess.className = 'modal modal_active';
 // };
